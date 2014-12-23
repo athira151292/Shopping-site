f167c8e5a5fafea186caf197a9b42ce89ddab209
athira151292 <athira@qburst.com>
2014-12-23 16:10:18 +0530

    Delete checkpass.js

--- checkpass.js
+++
@@ -1,7 +0,0 @@
-function check(pass,repass)
-	{
-		var $pass=document.getElementById(pass);
-		var $repass=document.getElementById(repass);
-		if($pass!=$repass)
-			alert("Passwords are different");
-	}
